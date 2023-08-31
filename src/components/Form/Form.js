eee1e9bcc8a1c0bdc97294504c8ebbf8b30b3396
Mukhtar <myl772ng@gmail.com>
2023-08-31 19:27:26 +0100

    modify test function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,43 +1,59 @@
-import React, { useState } from 'react'
-import './Form.css'
+import React, { useState } from "react";
+import "./Form.css";
 
 export default function Form({ addTask }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
 
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [dueDate, setDueDate] = useState('');
-
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(!title) return
-    const newTask = {
-      id: new Date().getTime(),
-      title,
-      description,
-      dueDate,
-      completed:false,
-      reminder:false
+    if (!title && !description && !dueDate) {
+      alert("Complete the form please");
+    } else {
+      const newTask = {
+        id: new Date().getTime(),
+        title,
+        description,
+        dueDate,
+        completed: false,
+        reminder: false,
+      };
+      addTask(newTask);
+      setTitle("");
+      setDescription("");
+      setDueDate("");
     }
-    addTask(newTask)
-    setTitle('');
-    setDescription('');
-    setDueDate('')
-  }
+  };
   return (
-    <div className='task-form'>
+    <div className="task-form">
       <h2>Add</h2>
       <form onSubmit={handleSubmit}>
-      <label htmlFor="Title">Title</label>
-      <input id='Title' type='text' value={title} onChange={(e)=> setTitle(e.target.value)} />
+        <label htmlFor="Title">Title</label>
+        <input
+          id="Title"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-      <label htmlFor="Description">Description</label>
-      <textarea id='Description' value={description} onChange={(e)=> setDescription(e.target.value)} />
+        <label htmlFor="Description">Description</label>
+        <textarea
+          id="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
-      <label htmlFor="Date">Date</label>
-      <input id='Date' type='date' value={dueDate} onChange={(e)=> setDueDate(e.target.value)} />
+        <label htmlFor="Date">Date</label>
+        <input
+          id="Date"
+          type="date"
+          value={dueDate}
+          onChange={(e) => setDueDate(e.target.value)}
+        />
 
-      <button type='submit'>Add Task</button>
+        <button type="submit">Add Task</button>
       </form>
     </div>
-  )
+  );
 }
